8b9e1c204c2c3e4d690172eaf6dcafff78ea5fae
Jay Pignata <john.pignata@gmail.com>
2008-08-18 00:28:57 -0400

    Fixed sample

--- samples/prospectpark_web.rb
+++ samples/prospectpark_web.rb
@@ -7,7 +7,7 @@ BOSSMan.application_id = "xXySTCfV34HiWg67Cwwym2mvf4tUfLJS3B73thD5ws_xFbLYh6uVvN
 
 prospectpark_web = BOSSMan::Search.web("prospect park", 0, 20)
 
-puts "Number of results: #{madmen_web.totalhits}"
+puts "Number of results: #{prospectpark_web.totalhits}"
 puts
 
 prospectpark_web.results.each do |result|
